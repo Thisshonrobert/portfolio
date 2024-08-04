0fc6320e87165cec0b80a8344f8bafd4a3fef762
Thisshonrobert <thisshonrobert0205@gmail.com>
2024-08-05 00:36:06 +0530

    scroll effect to about page added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,27 +1,69 @@
-import React from 'react';
+import { cn } from "../lib/utils";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
-function About() {
-    return (
-        <section name="About" className='bg-gradient-to-b from-blue-100 via-blue-200 to-blue-300 dark:from-gray-800 dark:via-black dark:to-black w-full md:h-screen'>
-            <div className='flex flex-col text-black dark:text-white justify-center mx-auto max-w-screen-lg p-4 w-full h-full'>
-                <div className='mt-20'>
-                    <h1 className='text-4xl inline border-b-4 font-bold border-blue-500 dark:border-gray-500'>About</h1>
-                </div>
-                
-                <p className='text-xl mt-6'>
-                    Thisshon Robert is a student and an aspiring web developer. He holds a degree in Electronics and Communication Engineering. His tech stack knowledge includes MongoDB, Express, React, Node, Typescript, Tailwind, Prisma, etc.
-                </p>
-                
-                <p className='text-xl mt-6'>
-                    He aspires to contribute more to the tech industry by solving client's problems. He also studied and passed the Japanese Language Proficiency Test N5. In the face of challenges within modern IT infrastructure, he brings forth a diverse skill set, ready to conquer and innovate.
-                </p>
-                
-                <p className='text-xl mt-6'>
-                    "If something doesn't work as expected, just <span className='text-pink-500 font-mono text-sm'>sudo</span> it!"
-                </p>
-            </div>
-        </section>
-    );
-}
+
+const TextRevealByWord = ({ text, className }) => {
+  const targetRef = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+  });
+  const words = text.split(" ");
+
+  return (
+    <div ref={targetRef} className={cn("relative z-0 h-[200vh]", className)}>
+      <div
+        className={
+          "sticky top-0 mx-auto flex h-[50%] max-w-4xl items-center bg-transparent px-[1rem] py-[5rem]"
+        }
+      >
+        <p
+          className={
+            "flex flex-wrap text-2xl font-bold text-black/20 dark:text-white/20 md:p-4 md:text-4xl lg:p-5 lg:text-4xl xl:text-7xl"
+          }
+        >
+          {words.map((word, i) => {
+            const start = i / words.length;
+            const end = start + 1 / words.length;
+            return (
+              <Word key={i} progress={scrollYProgress} range={[start, end]}>
+                {word}
+              </Word>
+            );
+          })}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const Word = ({ children, progress, range }) => {
+  const color = useTransform(progress, range, ["#00000033", "#FFFFFF"]); 
+  return (
+    <span className="xl:lg-3 relative mx-1 lg:mx-2.5">
+      <motion.span
+        style={{ color: color }}
+        className={"text-black dark:text-white"}
+      >
+        {children}
+      </motion.span>
+    </span>
+  );
+};
+
+const About = () => {
+  return (
+    <div name="About" className="mx-auto w-full h-full bg-gradient-to-b from-blue-100 via-blue-200 to-blue-300 dark:from-gray-800 dark:via-black dark:to-black p-12">
+      <div className="container mx-auto max-w-screen-lg  py-10 px-4 sm:px-6 lg:px-8">
+        <h1 className="text-4xl inline border-b-4 font-bold border-blue-500 dark:text-white dark:border-gray-500 mb-6">
+          About
+        </h1>
+        <TextRevealByWord className="lg:ml-32" text="I craft intuitive, responsive web experiences that seamlessly blend aesthetic design with functional elegance." />
+        
+      </div>
+    </div>
+  );
+};
 
 export default About;
